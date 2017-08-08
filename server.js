daf5a93c850477f5dd2e516c1fc59ce5499b195d
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-08 19:45:48 -0400

    now rendering data on home page

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const mustacheExpress = require("mustache-express");
-const pgPromise = require("pg-promise");
+const pgPromise = require("pg-promise")();
 const path = require("path");
 const expressValidator = require("express-validator");
-const data = require("./data.js");
 
 const app = express();
 
+const database = pgPromise({ database: "robots_db" });
+
 app.use(express.static("public"));
+// boiler-plate for validator
+app.use(expressValidator());
+// boiler-plate for mustache
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
 app.set("view engine", "mustache");
 
-app.get("/", function(req, res) {
-  res.render("robots_list", data);
+app.get("/", function(request, response) {
+  database.any("SELECT * FROM robots").then(rows => {
+    const data = {
+      robots: rows
+    };
+    response.render("robots_list", data);
+  });
 });
 
 app.get("/robots_details/:username", (request, response) => {
@@ -25,5 +34,5 @@ app.get("/robots_details/:username", (request, response) => {
 });
 
 app.listen(3000, function() {
-  console.log("Successfully accessed user directory!");
+  console.log("Successfully accessed robots directory!");
 });
